54acfedb2cc429df15d180197bc7da63b613263c
杨凌霄 <ylx@disanbo.com>
2018-10-24 11:14:43 +0800

    加了hello

---
+++ src/main/java/com/fuzamei/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.fuzamei.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fuzamei on 2018/10/24.
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
