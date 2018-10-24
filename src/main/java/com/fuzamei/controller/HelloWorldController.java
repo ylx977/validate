63bef589aba21f748e0182b90dd7b7a482136bd6
杨凌霄 <ylx@disanbo.com>
2018-10-24 11:47:28 +0800

    加了hello

--- src/main/java/com/fuzamei/controller/HelloWorldController.java
+++ src/main/java/com/fuzamei/controller/HelloWorldController.java
@@ -12,7 +12,7 @@ public class HelloWorldController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world version 4";
+        return "hello world version 4 ";
     }
 
 }
