3786080e54f80353151962293b2d467d2f9a3522
杨凌霄 <ylx@disanbo.com>
2018-11-05 22:45:56 +0800

    加了docker镜像

--- src/main/java/com/fuzamei/controller/HelloWorldController.java
+++ src/main/java/com/fuzamei/controller/HelloWorldController.java
@@ -12,7 +12,7 @@ public class HelloWorldController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world version 4.4";
+        return "hello world version 4.4 from docker container of 9000 port";
     }
 
     @RequestMapping("/hello2")
