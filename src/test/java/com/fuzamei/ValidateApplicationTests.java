a7936cb8361764e8a5b4238685a1a2413bc42dfe
杨凌霄 <ylx@disanbo.com>
2018-11-30 13:33:17 +0800

    jenkins 配置环境变量

--- src/test/java/com/fuzamei/ValidateApplicationTests.java
+++ src/test/java/com/fuzamei/ValidateApplicationTests.java
@@ -16,19 +16,19 @@ public class ValidateApplicationTests {
 	public void contextLoads() {
 	}
 
-	@Autowired
-	StringEncryptor encryptor;
-
-	@Test
-	public void getPass() {
-		String url = encryptor.encrypt("jdbc:mysql://172.16.100.14:3306/biginfo?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false&allowMultiQueries=true&useSSL=false");
-		String name = encryptor.encrypt("root");
-		String password = encryptor.encrypt("W%e>=&QhO87p");
-		System.out.println(url+"----------------");
-		System.out.println(name+"----------------");
-		System.out.println(password+"----------------");
-		Assert.assertTrue(name.length() > 0);
-		Assert.assertTrue(password.length() > 0);
-	}
+//	@Autowired
+//	StringEncryptor encryptor;
+//
+//	@Test
+//	public void getPass() {
+//		String url = encryptor.encrypt("jdbc:mysql://172.16.100.14:3306/biginfo?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false&allowMultiQueries=true&useSSL=false");
+//		String name = encryptor.encrypt("root");
+//		String password = encryptor.encrypt("W%e>=&QhO87p");
+//		System.out.println(url+"----------------");
+//		System.out.println(name+"----------------");
+//		System.out.println(password+"----------------");
+//		Assert.assertTrue(name.length() > 0);
+//		Assert.assertTrue(password.length() > 0);
+//	}
 
 }
